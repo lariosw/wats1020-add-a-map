aafa0faa914fb2cfaf4847166aa5d62832659a93
lariosw <lariosw@seattleu.edu>
2016-07-24 23:53:44 -0700

    latest updates

--- js/main.js
+++ js/main.js
@@ -19,8 +19,32 @@
 // interesting locations. (You'll need to figure out the latitude/longitude for
 // these locations using a mapping tool such as Google Maps.)
 
+$(document).ready(function() {
+   
+  //init map
+var mymap = L.map('map-container').setView([46.852, -121.760], 13);
 
+//get street map
+var streetMap = L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'});
 
+//get topography map
+var topoMap = L.tileLayer('http://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    maxZoom: 17,
+    attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+});
+
+//add layers to map
+var mapLayers = {
+    "Street Map": streetMap,
+    "Topography Map": topoMap
+};
+L.control.layers(mapLayers).addTo(mymap);
+
+//add default map
+streetMap.addTo(mymap);
+
+});
+    
 /* var map = L.map('map-container').setView([46.852, -121.760], 13);
 L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
